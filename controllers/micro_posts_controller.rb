9a53eb0f02c394be7fae18659a53f7a973b005da
alexcode18 <aschattner@gmail.com>
2014-11-09 14:02:13 -0500

    Completed the create new post with working tag adding.

--- controllers/micro_posts_controller.rb
+++ controllers/micro_posts_controller.rb
@@ -8,18 +8,23 @@ get '/posts' do
 	erb :'/posts/index'
 end
 
-# get '/posts/new' do
-# 	@micro_posts = MicroPost.all
-# 	erb :'/posts/new'
-# end
+get '/posts/new' do
+	@tags = Tag.all
+	@authors = Author.all
+	erb :'/posts/new'
+end
 
 get '/posts/:id' do
 	@micro_post = MicroPost.find(params[:id])
 	erb :'/posts/show'
 end
 
-post '/posts/' do
+post '/posts' do
 	micro_post = MicroPost.new(params[:micro_post])
+	tags = Tag.find(params[:tags])
+
+	tags.each { |tag| micro_post.tags << tag }
+
 	if micro_post.save
 		redirect("/posts/#{micro_post.id}")
 	else
